9e2ac9850ed26cd97eb21484e0f6e7bcc1262899
twoamdev <97489062+twoamdev@users.noreply.github.com>
2026-01-02 21:47:37 -0500

    base reactflow installed and running

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
 import './App.css'
+import { useState, useCallback } from 'react'
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge,
+  type Node,
+  type Edge,
+  type OnNodesChange,
+  type OnEdgesChange,
+  type OnConnect,
+} from '@xyflow/react'
 
+const initialNodes : Node[] =[
+  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges : Edge[] = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+ 
 function App() {
-
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+ 
+  const onNodesChange : OnNodesChange = useCallback(
+    (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange : OnEdgesChange = useCallback(
+    (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect : OnConnect = useCallback(
+    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+ 
   return (
     <>
-       <h1 className="text-2xl font-bold underline">
-    Hello world!
-  </h1>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      />
+    </div>
     </>
-  )
+  );
 }
+
+
+
+
+
+
+
+
+
 
 export default App
